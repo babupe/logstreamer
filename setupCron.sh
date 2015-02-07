758ea4b492b66e2b3a51b0ba31fb5ef5d416ac7a
Elumalai <babupe@a82066444001.ant.amazon.com>
2015-02-06 19:59:15 -0800

    removed setupcron

--- setupCron.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "*/1 * * * * ./logstreamer.py -d logstreamerconfig" > /tmp/crontab
-echo "*/5 * * * * ./configManager.py -d logstreamerconfig -q logstreamerconfig" >> /tmp/crontab
-echo "*/2 * * * * ./setupJobs.py -d logstreamerconfig" >> /tmp/crontab
-crontab /tmp/crontab
