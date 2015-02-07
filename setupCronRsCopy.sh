847cfc526c0706f6ef8e592b0b347dbf25b4a6ee
Elumalai <babupe@a82066444001.ant.amazon.com>
2015-02-06 19:57:32 -0800

    Remove unnecessary files and modified appsec to have only streamer

--- setupCronRsCopy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "*/5 * * * * ./configManager.py -d logstreamerconfig -q logstreamerconfig" > /tmp/rscopycrontab
-echo "*/15 * * * * ./copyToRS.py -d logstreamerconfig" >> /tmp/rscopycrontab
-crontab /tmp/rscopycrontab
